c1f8d453043f1df1e9d2ad745374f858e4c7c2a3
OgataiKhan <badellino.kevin@gmail.com>
2023-11-10 11:18:56 +0100

    feat: add snack2 comments

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
+// I ask the user for two words and save them as variables
 const wordOne = prompt('Type a word');
 const wordTwo = prompt('Type another word');
 
+// I use an if condition to print the shorter one first
 if (wordOne.length > wordTwo.length) {
     console.log(wordTwo, wordOne);
 } else {
