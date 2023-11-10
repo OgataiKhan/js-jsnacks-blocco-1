10ac31bcf49a974d2f403d6eedceb677aafe4868
OgataiKhan <badellino.kevin@gmail.com>
2023-11-10 11:24:25 +0100

    feat: add same length message

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -7,6 +7,8 @@ const wordTwo = prompt('Type another word');
 // I use an if condition to print the shorter one first
 if (wordOne.length > wordTwo.length) {
     console.log(wordTwo, wordOne);
-} else {
+} else if (wordTwo.length > wordOne.length) {
     console.log(wordOne, wordTwo);
+} else {
+    console.log(`The words ${wordOne} and ${wordTwo} are the same length.`);
 }
