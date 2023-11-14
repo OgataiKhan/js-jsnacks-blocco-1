953d22ce2c9cc2e153178227c150da7ae8849d3d
OgataiKhan <badellino.kevin@gmail.com>
2023-11-14 09:56:33 +0100

    feat: loops

---
+++ snack4/js/main.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/*
+const numbersArray = []; // Create an empty array
+let sum = 0; // Initialize an empty variable to keep track of the total sum
+
+// Use a while loop to keep asking for numbers until the sum matches or exceeds 50
+while (sum < 50) {
+    let number = parseFloat(prompt('Please insert a number')); // Ask the user to input a number
+    numbersArray.push(number); // Push the number into the array
+    sum += number; // Add the number to the sum
+    console.log(`Array: ${numbersArray}`); // Print the array on each iteration
+    console.log(`Sum: ${sum}`); // Print the total sum on each iteration
+}
+
+console.log('The total sum is now equal or greater than 50.');
+*/
+
+/*
+const numbersArray2 = [];
+let sum2 = 0;
+
+do {
+    let number2 = parseFloat(prompt('Please insert a number'));
+    numbersArray2.push(number2);
+    sum2 += number2;
+    console.log(`Array: ${numbersArray2}`);
+    console.log(`Sum: ${sum2}`);
+} while (sum2 < 50);
+
+console.log('The total sum is now equal or greater than 50.');
+*/
+
+const numbersArray3 = [];
+let sum3 = 0;
+
